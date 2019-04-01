b17571c11cd962f8eef474891b02751118e57855
jonathan gerber <jlgerber@gmail.com>
2019-04-01 14:16:30 -0700

    adds env control for logging in example

--- example/logger.py
+++ example/logger.py
@@ -6,67 +6,81 @@ Implementations of logger. We have ColoredLogger and Logger
 from example.context import service_locator
 from example.services import BaseLogger
 from example.color_util import (ColorCM, printit)
-
+import os
 
 __all__ = ("Logger", "BaseLogger")
+
+LEVELS = {"debug": 10, "info" : 20, "warn": 30}
+def lookup_level(level):
+    if isinstance(level, int):
+        return level
+    return LEVELS.get(level,10)
 
 class Logger(BaseLogger):
     """
     Baseic logger implementation. Nothing fancy.
     """
-    def __init__(self, name):
+    def __init__(self, name, level=os.environ.get("LOGGING_LEVEL", "debug")):
         """
         set the name
         """
         self.name = name
+        self.level = lookup_level(level)
 
     def debug(self, *args):
         """
         trivial debug impl
         """
-        print "DEBUG | {} |".format(self.name), " ".join(args)
+        if self.level <= lookup_level("debug"):
+            print "DEBUG | {} |".format(self.name), " ".join(args)
 
     def info(self, *args):
         """
         trivial info impl
         """
-        print "INFO  | {} |".format(self.name), " ".join(args)
+        if self.level <= lookup_level("info"):
+            print "INFO  | {} |".format(self.name), " ".join(args)
 
     def warn(self, *args):
         """
         trivial warn impl
         """
-        print "WARN  | {} |".format(self.name), " ".join(args)
+        if self.level <= lookup_level("warn"):
+            print "WARN  | {} |".format(self.name), " ".join(args)
 
 
 class ColorLogger(BaseLogger):
     """
     Colored logger implementation. Like Logger but in color.
     """
-    def __init__(self, name):
+    def __init__(self, name, level=os.environ.get("LOGGING_LEVEL", "debug")):
         """
         set the name
         """
         self.name = name
+        self.level = lookup_level(level)
 
     def debug(self, *args):
         """
         trivial debug impl
         """
-        with ColorCM(printit, "blue") as colorm:
-            colorm("DEBUG | {} |".format(self.name), *args)
+        if self.level <= lookup_level("debug"):
+            with ColorCM(printit, "blue") as colorm:
+                colorm("DEBUG | {} |".format(self.name), *args)
 
     def info(self, *args):
         """
         trivial info impl
         """
-        with ColorCM(printit, "green") as colorm:
-            colorm("INFO  | {} |".format(self.name), " ".join(args))
+        if self.level <= lookup_level("info"):
+            with ColorCM(printit, "green") as colorm:
+                colorm("INFO  | {} |".format(self.name), " ".join(args))
 
     def warn(self, *args):
         """
         trivial warn impl
         """
-        with ColorCM(printit, "red") as colorm:
-            colorm("WARN  | {} |".format(self.name), " ".join(args))
+        if self.level <= lookup_level("warn"):
+            with ColorCM(printit, "red") as colorm:
+                colorm("WARN  | {} |".format(self.name), " ".join(args))
 
